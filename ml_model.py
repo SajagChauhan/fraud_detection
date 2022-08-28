6617b182e2261a431f8b2bb6d617fbfa0dcb6f70
SajagChauhan <sajagchauhan7@gmail.com>
2022-08-28 21:26:01 +0530

    added 3 new model codes

---
+++ ml_model.py
@@ -0,0 +1,15 @@
+
+
+
+print("adding all l related code here "  )
+
+def model1():
+    print("adding code for ml model1")
+
+
+def model2():
+    print("adding code for ml model2")
+
+
+def model3():
+    print("adding code for ml model3")
